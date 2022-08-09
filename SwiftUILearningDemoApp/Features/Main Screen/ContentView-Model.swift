7008d84db81fecd419a00bfb1403629bf1ec4bcd
Imran Sayeed <imran.sayeed@tigerit.com>
2022-08-10 00:41:23 +0600

    Added Grid layout with sample data with MVVM code structure

--- SwiftUILearningDemoApp/Features/Main Screen/ContentView-Model.swift
+++ SwiftUILearningDemoApp/Features/Main Screen/ContentView-Model.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct AllAppsModel: Identifiable {
+    let id = UUID().uuidString
+    let name: String
+    let imageName: String
+}
+
+extension AllAppsModel {
+    static func sampleApps() -> [AllAppsModel] {
+        [
+            AllAppsModel(name: "Restart", imageName: "restart"),
+            AllAppsModel(name: "Res", imageName: "restart"),
+            AllAppsModel(name: "Restart & som", imageName: "restart"),
+            AllAppsModel(name: "Restart & other", imageName: "restart"),
+        ]
+    }
+}
--- SwiftUILearningDemoApp/Features/Main Screen/ContentView.swift
+++ SwiftUILearningDemoApp/Features/Main Screen/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let data: [AllAppsModel] = AllAppsModel.sampleApps()
+    var colors: [Color] = [.red, .green, .blue, .yellow]
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                    ForEach(data, id: \.id) { item in
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 170, height: 180)
+                                .foregroundColor(.yellow)
+                                .cornerRadius(30)
+                            Text("\(item.name)")
+                                .foregroundColor(.white)
+                                .font(.system(.title, design: .rounded))
+                                .padding(8)
+                                .multilineTextAlignment(.center)
+                        }
+                    }
+                }
+            }
             .padding()
+            .navigationTitle("All Apps")
+            .navigationBarTitleDisplayMode(.large)
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
