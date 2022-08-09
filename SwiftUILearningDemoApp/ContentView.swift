379693b2139bf3dd5b70cbf69e6d3ee66211c5d6
Imran Sayeed <imran.sayeed@tigerit.com>
2022-08-09 22:26:31 +0600

    Initial swiftui project

---
+++ SwiftUILearningDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILearningDemoApp
+//
+//  Created by Imran Sayeed on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILearningDemoApp/SwiftUILearningDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningDemoAppApp.swift
+//  SwiftUILearningDemoApp
+//
+//  Created by Imran Sayeed on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
